c433114ede928c389496b4125d8f790f4c8059de
VaishnaviChatur <92914588+VaishnaviChatur@users.noreply.github.com>
2023-06-20 22:48:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,87 +2,36 @@ import React,{useState} from "react";
 import "./style.css";
 
 export default function App() {
-  const[value,setValue] = useState()
-  const [todo, setTodo] = useState([])
-  const [todoEditing, setTodoEditing] = useState(null)
-  const[editingText, setEditingText] = useState("")
-
-  function handleinputchange(e){
-    setValue(e.target.value)
-  }
-  function handleinput(){
-    if(value != '' && todo !== ''){
-      setTodo([...todo,  value]);
-      setValue('')
-      
-
-    }
-  };
-  function handleDelete(index){
-    let new1 = [...todo]
-    new1.splice(index , 1)
-    setTodo([...new1])
-  }
- 
-  function editTodo(index){
-    const updatedTodos = [...todo];
-    updatedTodos[index] = editingText;
-    setTodo(updatedTodos);
-    setTodoEditing(null);
-    setEditingText("");
-
-  }
+ const [data, setData] = useState([
+    { id: 1, name: 'ram' },
+    { id: 2, name: 'sita' },
+    { id: 3, name: 'hanuman' },
+    { id: 4, name: 'lakshman' },
+    { id: 5, name: 'bharat' },
+    { id: 6, name: 'shatrughn' },
   
-//   const markTodo = index => {
-//     const newTodos = [...todo];
-//     newTodos[index].isDone = true;
-//     setTodo(newTodos);
-// };
+  ]);
+   const [search, setSearch] = useState('');
+
 
   return (
     <div>
-    enter your name <input 
-    value = {value} 
-
-    onChange = {handleinputchange} />
-     <button onClick = {handleinput}>Add list</button>
-     
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={e=>setSearch(e.target.value)}
+      />
+      <ul>
+        {data
+          .filter((item) =>
+          item.name.toLowerCase().includes(search.toLowerCase())
+        )
+          .map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+      </ul>
     
-     <div>
-       {
-         todo.map((todos,index) =>  <div key={index}>
-         {
-           todoEditing === index ? (<input 
-            type="text"
-            onChange={(e) => setEditingText(e.target.value)} 
-            value={editingText}
-            />) : ( <p style={{ textDecoration: todo.isDone ? "line-through" : "" }}> {todos} </p>)}
-      
-      <button onClick={() => markTodo(index)}>✓</button>
-          
-           <button onClick={() => handleDelete(index)}> Delete </button>
-
-           {
-             todoEditing === index ? (
-              <button onClick={() => editTodo(index)}> Submit Edits</button>
-             ) : (
-             <button onClick={() => setTodoEditing(index)}>Edit Todo</button>
-             )
-           }
-          
-          
-          
-          </div>
-         
-          )
-          
-
-       }
-
-     
- 
-
-       </div>
     </div>
   );
 }
